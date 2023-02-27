7a1d8fbba95b4a4f69ca51ffd59b97de54523cfe
Niklas Lübcke <niklas.lubcke@gmail.com>
2023-02-27 08:31:44 +0100

    Another build using the back-faceing camera.

---
+++ test-mp_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FaceMarker::Awake()
+extern void FaceMarker_Awake_m212DEF6DF5BB1D7D5289D52FB0D5CB2B6D6E31CE (void);
+// 0x00000002 System.Void FaceMarker::OnEnable()
+extern void FaceMarker_OnEnable_m9EFBAE409A95130C0797BB19D4299B177369F3D0 (void);
+// 0x00000003 System.Void FaceMarker::OnDisable()
+extern void FaceMarker_OnDisable_mFD5FC1D87B8CFABB7156BC07C9DB9CEAD23F2F17 (void);
+// 0x00000004 System.Void FaceMarker::OnFacesChanged(UnityEngine.XR.ARFoundation.ARFacesChangedEventArgs)
+extern void FaceMarker_OnFacesChanged_m2B355103B542DD00A5A6926CA3638B80C9F2805A (void);
+// 0x00000005 System.Void FaceMarker::.ctor()
+extern void FaceMarker__ctor_mF01BB0074C2E7FF11717F42CCE8D8E320CEFBF61 (void);
+// 0x00000006 System.Void SceneTemplate_RotateCube::Update()
+extern void SceneTemplate_RotateCube_Update_mAF2FB34B99E325443A80D60A7EB73DC092EAE9A6 (void);
+// 0x00000007 System.Void SceneTemplate_RotateCube::.ctor()
+extern void SceneTemplate_RotateCube__ctor_mC7E11C05D25FFF6F8D7EA06AAE5598B497365CD2 (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	FaceMarker_Awake_m212DEF6DF5BB1D7D5289D52FB0D5CB2B6D6E31CE,
+	FaceMarker_OnEnable_m9EFBAE409A95130C0797BB19D4299B177369F3D0,
+	FaceMarker_OnDisable_mFD5FC1D87B8CFABB7156BC07C9DB9CEAD23F2F17,
+	FaceMarker_OnFacesChanged_m2B355103B542DD00A5A6926CA3638B80C9F2805A,
+	FaceMarker__ctor_mF01BB0074C2E7FF11717F42CCE8D8E320CEFBF61,
+	SceneTemplate_RotateCube_Update_mAF2FB34B99E325443A80D60A7EB73DC092EAE9A6,
+	SceneTemplate_RotateCube__ctor_mC7E11C05D25FFF6F8D7EA06AAE5598B497365CD2,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	6340,
+	6340,
+	6340,
+	4943,
+	6340,
+	6340,
+	6340,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
